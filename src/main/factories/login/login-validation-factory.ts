afd09e4bc9e96500644ee94993d0930c774d5ca1
Tiago Pereira <tiagohcp@gmail.com>
2020-08-27 14:05:55 -0300

    refactor: fix loginValitation fields check

--- src/main/factories/login/login-validation-factory.ts
+++ src/main/factories/login/login-validation-factory.ts
@@ -4,7 +4,7 @@ import { EmailValidatorAdapter } from '../../../main/adapters/validators/email-v
 
 export const makeLoginValidation = (): ValidationComposite => {
   const validations: Validation[] = []
-  for (const field of ['name', 'email']) {
+  for (const field of ['email', 'password']) {
     validations.push(new RequiredFieldValidation(field))
   }
   validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
